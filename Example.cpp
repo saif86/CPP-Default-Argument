bc72af9e3a9862f3f85bc05f21b8c65185d334a7
Saif Ijaz <41892175+saif86@users.noreply.github.com>
2018-12-20 13:49:10 +0800

    Update Example.cpp

--- Example.cpp
+++ Example.cpp
@@ -1,10 +1,28 @@
-/* C++ Program to demonstrate working of default argument */
+/**
+* @file Example2.cpp
+*
+* @brief C++ Program to demonstrate working of default argument.
+*
+* @author Saif Ullah Ijaz
+*
+*/
 
 #include <iostream> 
 using namespace std;
 
+// FUNCTION PROTOTYPE (DECLARATION)
+
+/** function that prints a character multiple times.
+*
+* @param c The input character to be printed.
+* @param n The no. of times to be printed.
+*
+* @return void.
+*/
 void display(char = '*', int = 1);
 
+
+// function main begins program execution
 int main() {
 	cout << "No argument passed:\n";
 	display();
@@ -18,10 +36,16 @@ int main() {
 	system("pause");
 	return 0;
 }
+// end main 
 
+// FUNCTION DEFINITION
+
+// function that displays the input character desired no. of times
 void display(char c, int n) {
 	for (int i = 1; i <= n; ++i) {
 		cout << c;
 	}
 	cout << endl;
 }
+// end function display
+
